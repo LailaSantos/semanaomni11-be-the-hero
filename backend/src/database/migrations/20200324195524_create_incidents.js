7a821dd08392cce91b85d5e618ff44622d627d5f
Lailaa <lailaroberta100@gmail.com>
2020-03-26 11:28:37 -0300

    back

--- backend/src/database/migrations/20200324195524_create_incidents.js
+++ backend/src/database/migrations/20200324195524_create_incidents.js
@@ -1,18 +1,18 @@
 
 exports.up = function(knex) {
 	return 	knex.schema.createTable('incidents', function(table) {
-		table.increments(); //primary key com incremento
+		table.increments(); 
 		table.string('title').notNullable();
 		table.string('description').notNullable();
 		table.decimal('value').notNullable();
 		
-		table.string('ong_id').notNullable(); //relacionamento bd
+		table.string('ong_id').notNullable();
 
 		table.foreign('ong_id').references('id').inTable('ongs');
 });
 
 };
 
-exports.down = function(knex) { //se deu algum problema eu faço isso...
+exports.down = function(knex) { 
  return  knex.schema.dropTable('incidents');
 };
